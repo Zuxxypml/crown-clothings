2700c9d8b71f08294a1a19ba0ad231038161e246
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-05-30 21:14:00 +0100

    created app and added homepage

---
+++ src/Components/App/App.jsx
@@ -0,0 +1,12 @@
+import React, { Component } from "react";
+import Homepage from "../Homepage/Homepage";
+import './App.css'
+class App extends Component {
+  render() {
+    return <div>
+        <Homepage />
+    </div>;
+  }
+}
+
+export default App;
