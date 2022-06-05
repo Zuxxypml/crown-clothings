b06f48aee65cc2a7aef7c2037176a42f8b927f60
IAMAGODDAMNHACKER <70457708+IAMAGODDAMNHACKER@users.noreply.github.com>
2022-06-05 07:15:22 +0100

    modified cartDropdown to show cart Items

--- src/Components/CartDropdown/CartDropdown.jsx
+++ src/Components/CartDropdown/CartDropdown.jsx
@@ -1,14 +1,22 @@
 import React from "react";
+import { connect } from "react-redux";
+import CartItem from "../CartItem/CartItem";
 import CustomButton from "../CustomButton/CustomButton";
 import "./CartDropdown.scss";
 
-function CartDropdown() {
+function CartDropdown({ cartItems }) {
   return (
     <div className="cart-dropdown">
-      <div className="cart-items"></div>
+      <div className="cart-items">
+        {cartItems.map((cartItem) => {
+          return <CartItem key={cartItem.id} item={cartItem} />;
+        })}
+      </div>
       <CustomButton className="custom-button">CHECKOUT</CustomButton>
     </div>
   );
 }
-
-export default CartDropdown;
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+  cartItems,
+});
+export default connect(mapStateToProps)(CartDropdown);
---
+++ src/Components/CartItem/CartItem.jsx
@@ -0,0 +1,18 @@
+import "./CartItem.scss";
+import React from "react";
+
+function CartItem({ item: { imageUrl, name, price, quantity } }) {
+  return (
+    <div className="cart-item">
+      <img src={imageUrl} alt={name} />
+      <div className="item-details">
+        <span className="name">{name}</span>
+        <span className="price">
+          {quantity} x ${price}
+        </span>
+      </div>
+    </div>
+  );
+}
+
+export default CartItem;
